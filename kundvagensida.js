d233453731e4610086190af11615dda8d3e19fad
anukhatra <55985079+anukhatra@users.noreply.github.com>
2020-04-27 15:05:39 +0200

    Add files via upload

---
+++ kundvagensida.js
@@ -0,0 +1,192 @@
+/** Get products from the localstorage and return the list */
+function getCartItems() {
+    var userList = JSON.parse(localStorage.getItem('userList'))
+    var userIndex = JSON.parse(localStorage.getItem('index'))
+    var retrievedData = userList[userIndex].products
+    if (!retrievedData) { return [] }
+    return retrievedData
+}
+
+function initSite() {
+    addProductsToWebpage()
+    loginInit()
+}
+
+/** Uses the loaded products data to create a visible product list on the website */
+
+function addProductsToWebpage() {
+    var userIndex = JSON.parse(localStorage.getItem('index'))
+    var userList = JSON.parse(localStorage.getItem('userList'))
+
+    // Check your console to see that the products are stored in the listOfProducts varible.
+    function changeCounterColor() {
+        if (getCartItems().length !== 0) {
+            document.getElementById("counter").style.backgroundColor = "#E64E4E"
+            cartItemsTitle.innerText = "Kundvagen"
+        } else {
+            document.getElementById("counter").style.backgroundColor = "#F5F5F5"
+            cartItemsTitle.innerText = "Vagnen är tom"
+        }
+    }
+    document.getElementById("counter").innerHTML = getCartItems().length
+    if( document.getElementById("counter").innerHTML == null || document.getElementById("counter").innerHTML == 0 ){
+        document.getElementById("counter").style.backgroundColor = "#F5F5F5"
+    }else{
+        document.getElementById("counter").style.backgroundColor = "#E64E4E"
+    }
+    var totalPrice = 0
+    var cartItems = getCartItems()
+
+    var body = document.getElementsByTagName("body")[0]
+    var container = document.getElementsByTagName("main")[0]
+    container.innerHTML = ''
+    var shopItems = document.createElement("div")
+
+    var cartItemsTitleContainer = document.createElement("div")
+    var cartItemsTitle = document.createElement("div")
+    cartItemsTitle.classList = "cartItemsTitle"
+    var cartItemsTitleIcon = document.createElement("i")
+
+    var closedPurchase = document.createElement("i")
+    closedPurchase.innerText = "Slut för dit köp"
+    closedPurchase.classList = "fas fa-trash-alt" + " " + "btn" + " " + "closedPurchaseBtn" + " " + "cart-item-button "
+    closedPurchase.onclick = function() {
+        deletedData(this.data)
+    }
+
+    cartItemsTitleIcon.classList = "fas fa-shopping-cart"
+    cartItemsTitleContainer.classList = "cartItemsTitleContainer"
+    if( document.getElementById("counter").innerHTML == null ||
+    document.getElementById("counter").innerHTML == 0 ){
+       cartItemsTitle.innerText = "Vagnen är tom";
+       }else{
+       cartItemsTitle.innerText = "Kundvagen"
+    }
+
+    container.classList = "cartContainer" + " " + "content-section"
+    shopItems.classList = "cart-items"
+    changeCounterColor()
+    for (var i = 0; i < cartItems.length; i++) {
+        totalPrice += cartItems[i].price
+        var shopItem = document.createElement("div")
+        shopItem.classList = "cart-item"
+
+        var shopItemTitle = document.createElement("span")
+        shopItemTitle.classList = "shop-item-title"
+
+        var shopItemImage = document.createElement("IMG")
+        shopItemImage.classList = "cart-item-image"
+        shopItemImage.setAttribute("src", "./assets/" + cartItems[i].image)
+        shopItemImage.setAttribute("width", "200")
+        shopItemImage.setAttribute("height", "320")
+        shopItemImage.setAttribute("alt", "The Pulpit Rock")
+
+        var shopItemDetails = document.createElement("div")
+        shopItemDetails.classList = "cart-item-details"
+
+        var shopItemPrice = document.createElement("span")
+        shopItemPrice.classList = "cart-item-price"
+
+        var taBortButton = document.createElement("i")
+        taBortButton.innerText = "Ta bort"
+        taBortButton.data = i
+        taBortButton.classList = "fas fa-trash-alt" + " " + "btn" + " " + "cart-btn-primary" + " " + "cart-item-button"
+        taBortButton.onclick = function() {
+            removeItem(this.data)
+        }
+
+        var totalCartItemsPrice = document.createElement("div")
+        totalCartItemsPrice.classList = "totalCartItemsPrice"
+        totalCartItemsPrice.innerText = "Totalt pris: " + totalPrice + " kr"
+
+        shopItemTitle.innerText = cartItems[i].title
+        shopItemImage.innerText = "./images" + cartItems[i].image
+        shopItemPrice.innerText = cartItems[i].price + " " + "kr"
+        cartItems[i].price = totalPrice
+
+        shopItem.appendChild(shopItemDetails)
+
+        shopItemDetails.appendChild(shopItemImage)
+        shopItemDetails.appendChild(shopItemTitle)
+        shopItemDetails.appendChild(shopItemPrice)
+
+        shopItem.appendChild(taBortButton)
+
+        shopItems.appendChild(shopItem)
+        userList[userIndex].total = totalPrice
+        localStorage.setItem("userList", JSON.stringify(userList))
+
+    }
+    cartItemsTitleContainer.appendChild(cartItemsTitle)
+    container.appendChild(cartItemsTitleContainer)
+    container.appendChild(shopItems)
+    container.appendChild(totalCartItemsPrice)
+    container.appendChild(closedPurchase)
+    body.appendChild(container)
+
+    // Add your code here, remember to brake your code in to smaller function blocks
+    // to reduce complexity and increase readability. Each function should have
+    // an explainetory comment like the one for this function, see row 22.
+    // TODO: Remove the console.log and these comments when you've read them.
+}
+
+function removeItem(index) {
+    var userIndex = JSON.parse(localStorage.getItem('index'))
+    var userList = JSON.parse(localStorage.getItem('userList'))
+    userList[userIndex].products.splice(index, 1)
+    localStorage.setItem("userList", JSON.stringify(userList))
+    if (userList[userIndex].products.length == 0) {
+        userList[userIndex].total = 0
+        localStorage.setItem("userList", JSON.stringify(userList))
+    }
+    addProductsToWebpage()
+}
+
+function deletedData() {
+    var userIndex = JSON.parse(localStorage.getItem('index'))
+    var userList = JSON.parse(localStorage.getItem('userList'))
+    userList[userIndex].products.splice(0, userList[userIndex].products.length)
+    userList[userIndex].total = 0
+    localStorage.setItem("userList", JSON.stringify(userList))
+    window.location.pathname = 'index.html'
+    alert("Ditt köp har bekräftats")
+}
+
+function homePage() {
+    var index = JSON.parse(localStorage.getItem('index'))
+    var userList = JSON.parse(localStorage.getItem('userList'))
+    document.getElementById("counter").innerHTML = userList[index].products.length
+    changeCounterColor()
+}
+
+function loginInit() {
+    var index = JSON.parse(localStorage.getItem('index'))
+    var userList = JSON.parse(localStorage.getItem('userList'))
+    var checkLogin = "Logga in"
+    if (checkLoginCase() == checkLogin) {
+        document.getElementById("counter").innerText = 0
+    } else {
+        document.getElementById("counter").innerText = userList[index].products.length
+        changeCounterColor()
+    }
+}
+
+function login1() {
+    var checkLogin = "Logga in"
+    localStorage.setItem("checkLogin", JSON.stringify(checkLogin))
+    window.location.pathname = 'index.html'
+    changeCounterColor()
+}
+
+function checkLoginCase() {
+    var checkCaseOfLogin = JSON.parse(localStorage.getItem('checkLogin'))
+    return checkCaseOfLogin
+}
+
+function changeCounterColor() {
+    if (parseInt(document.getElementById("counter").innerHTML) != 0) {
+        document.getElementById("counter").style.backgroundColor = "#E64E4E"
+    } else {
+        document.getElementById("counter").style.backgroundColor = "#F5F5F5"
+    }
+}
---
+++ main.js
@@ -0,0 +1,267 @@
+var listOfProducts
+var total = 0
+var index = JSON.parse(localStorage.getItem('index'))
+var goToProductsPageStatus
+
+
+function loadProducts() {
+    fetch("./products.json")
+        .then(function(response) {
+            return response.json()
+        })
+        .then(function(products) {
+            listOfProducts = products
+            addProductsToWebpage()
+        })
+}
+
+function initSite() {
+    loadProducts()
+    loginInit()
+
+    loadProducts();
+    var count = JSON.parse(localStorage.getItem('listOfProducts')).length
+    document.getElementById("counter").innerHTML = count
+    if (count == null || count == 0) {
+        document.getElementById("counter").style.backgroundColor = "#F5F5F5"
+    } else {
+        document.getElementById("counter").style.backgroundColor = "#E64E4E"
+    }
+}
+
+function addProductsToWebpage() {
+    for (var i = 0; i < listOfProducts.length; i++) {
+        var shopItems = createMobileCard(listOfProducts[i])
+        document.getElementById("container").appendChild(shopItems)
+    }
+    changeCounterColor()
+}
+
+function createMobileCard(listOfProducts) {
+    var shopItems = document.createElement("div")
+    shopItems.classList = "shop-item" + " " + "content-section"
+
+    var shopItemTitle = document.createElement("span")
+    shopItemTitle.classList = "shop-item-title"
+
+    var shopItemDescription = document.createElement("p")
+    shopItemDescription.classList = "shop-item-description"
+
+    var shopItemImage = document.createElement("IMG")
+    shopItemImage.classList = "shop-item-image"
+    shopItemImage.setAttribute("src", "./assets/" + listOfProducts.image)
+    shopItemImage.setAttribute("width", "200")
+    shopItemImage.setAttribute("height", "320")
+    shopItemImage.setAttribute("alt", "The Pulpit Rock")
+
+    var shopItemDetails = document.createElement("div")
+    shopItemDetails.classList = "shop-item-details"
+
+    var shopItemPrice = document.createElement("span")
+    shopItemPrice.classList = "shop-item-price"
+
+    shopItemTitle.innerText = listOfProducts.title
+    shopItemDescription.innerText = listOfProducts.description
+    shopItemImage.innerText = "./images" + listOfProducts.image
+    shopItemPrice.innerText = listOfProducts.price + " " + "kr"
+
+    var shopItemButton = document.createElement("i")
+    shopItemButton.innerText = "Lägg till i kundvagn"
+    shopItemButton.data = listOfProducts
+    shopItemButton.classList = "fas fa-shopping-cart" + " " + "btn" + " " + "btn-primary" + " " + "shop-item-button"
+    shopItemButton.onclick = function() {
+        addData(this.data)
+    }
+    shopItems.appendChild(shopItemTitle)
+    shopItems.appendChild(shopItemDescription)
+    shopItems.appendChild(shopItemImage)
+    shopItemDetails.appendChild(shopItemPrice)
+    shopItemDetails.appendChild(shopItemButton)
+    shopItems.appendChild(shopItemDetails)
+    return shopItems
+}
+
+
+function addData(product) {
+    var index = JSON.parse(localStorage.getItem('index'))
+    var userList = JSON.parse(localStorage.getItem('userList'))
+    var checkCaseOfLogin = document.getElementById("signIn").innerText
+    var modalSignin = document.getElementById('modalSignin')
+    var cart = JSON.parse(localStorage.getItem('listOfProducts'))
+
+    if (!cart) {
+        cart = []
+    }
+
+    if (!userList) {
+        userList = []
+    }
+
+    if (checkCaseOfLogin == "Logga in") {
+        modalSignin.style.display = "block"
+
+    } else {
+        cart.push(product)
+        total += parseInt(product.price)
+        userList[index].total = total
+        userList[index].products.push(product)
+        localStorage.setItem("userList", JSON.stringify(userList))
+        document.getElementById("counter").innerHTML = userList[index].products.length
+        changeCounterColor()
+    }
+}
+
+function GeneratePass() {
+    var length = 4
+    var result = ''
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+    var charactersLength = characters.length
+    for (var i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength))
+    }
+    return result
+}
+
+function register() {
+    var index = JSON.parse(localStorage.getItem('index'))
+    var userList = JSON.parse(localStorage.getItem('userList'))
+    var username = document.getElementById("regUsername").value
+    var password = GeneratePass()
+    var email = document.getElementById("RegEmail").value
+    var modalRegister = document.getElementById('modalRegister')
+    var accountExists = false
+
+    if (!userList) {
+        userList = []
+    }
+    for (var i = 0; i < userList.length; i++) {
+        if (username == userList[i].username) {
+            accountExists = true
+            break
+        }
+    }
+    if (accountExists) {
+        // Konto finns
+        alert("Du har registrerat redan")
+    } else {
+        // Konto finns ej
+        userList.push({ username: username, password: password, email: email, total: 0, products: [] })
+        localStorage.setItem("userList", JSON.stringify(userList))
+        alert("Du har fått lösernord för att logga in:  " + password)
+        modalRegister.style.display = "none"
+    }
+}
+
+function login() {
+    var userIndex = JSON.parse(localStorage.getItem('index'))
+    var userList = JSON.parse(localStorage.getItem('userList'))
+    var username = document.getElementById("signUsername").value
+    var password = document.getElementById("signPassword").value
+    var modalSignin = document.getElementById('modalSignin')
+    var modalRegister = document.getElementById('modalRegister')
+    var accountExists = false
+    if (!userList) {
+        userList = []
+    }
+    for (var i = 0; i < userList.length; i++) {
+        if ((username == userList[i].username) && (password == userList[i].password)) {
+            accountExists = true
+            break
+        }
+    }
+
+
+
+    if (accountExists) {
+        //Konto finns
+        if (goToProductsPageStatus) {
+
+            if (userList[userIndex].products.length == 0) {
+                var checkLogin = "Logga ut"
+                document.getElementById("counter").innerHTML = userList[i].products.length
+                modalSignin.style.display = "none"
+                document.getElementById("signIn").innerText = checkLogin
+                localStorage.setItem("checkLogin", JSON.stringify(checkLogin))
+                localStorage.setItem("index", JSON.stringify(i))
+                goToProductsPageStatus = false
+                changeCounterColor()
+
+            } else {
+                var checkLogin = "Logga ut"
+                document.getElementById("counter").innerHTML = userList[i].products.length
+                modalSignin.style.display = "none"
+                document.getElementById("signIn").innerText = checkLogin
+                localStorage.setItem("checkLogin", JSON.stringify(checkLogin))
+                localStorage.setItem("index", JSON.stringify(i))
+                window.location.pathname = 'kundvagnsida.html'
+                changeCounterColor()
+                goToProductsPageStatus = false
+            }
+        } else {
+            var checkLogin = "Logga ut"
+            document.getElementById("counter").innerHTML = userList[i].products.length
+            modalSignin.style.display = "none"
+            document.getElementById("signIn").innerText = checkLogin
+            localStorage.setItem("checkLogin", JSON.stringify(checkLogin))
+            localStorage.setItem("index", JSON.stringify(i))
+            changeCounterColor()
+        }
+    } else {
+        modalRegister.style.display = "block"
+    }
+}
+
+function login1() {
+    var checkLogin = "Logga in"
+    if (checkLoginCase() == checkLogin) {
+        modalSignin.style.display = "block"
+    } else {
+        document.getElementById("counter").innerText = 0
+        document.getElementById("signIn").innerText = checkLogin
+        localStorage.setItem("checkLogin", JSON.stringify(checkLogin))
+        changeCounterColor()
+    }
+}
+
+function loginInit() {
+    var userList = JSON.parse(localStorage.getItem('userList'))
+    var checkLogin = "Logga in"
+    if (checkLoginCase() == checkLogin) {
+        document.getElementById("counter").innerText = 0
+    } else {
+        document.getElementById("counter").innerText = userList[index].products.length
+        document.getElementById("signIn").innerText = "Logga ut"
+        changeCounterColor()
+    }
+}
+
+function checkLoginCase() {
+    var checkCaseOfLogin = JSON.parse(localStorage.getItem('checkLogin'))
+    return checkCaseOfLogin
+}
+
+function goToProductspage() {
+    if (checkLoginCase() == "Logga in") {
+        modalSignin.style.display = "block"
+        goToProductsPageStatus = true
+    } else {
+        window.location.pathname = 'kundvagnsida.html'
+    }
+}
+
+function changeCounterColor() {
+    if (parseInt(document.getElementById("counter").innerHTML) != 0) {
+        document.getElementById("counter").style.backgroundColor = "#E64E4E"
+    } else {
+        document.getElementById("counter").style.backgroundColor = "#F5F5F5"
+    }
+}
+
+function getEventListener() {
+    document.addEventListener('click', function(e) {
+        e = e || window.event
+        var target = e.target || e.srcElement,
+            text = target.textContent || target.innerText
+    }, false)
+
+}
